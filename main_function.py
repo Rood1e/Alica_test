52731acb1d49a1dffdce2c3e9e62502aae754a1a
Yegor <44552545+Rood1e@users.noreply.github.com>
2019-04-20 14:45:15 +0300

    Add files via upload

--- main_function.py
+++ main_function.py
@@ -2,5 +2,7 @@ from little_function import *
 
 
 def handle_dialog(request, response, user_storage):
+    if not user_storage:
+        user_storage = {}
     output_message = "hello"
     return message_return(response, user_storage, output_message)
