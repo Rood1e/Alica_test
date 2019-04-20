6d8e262dfc16c952139e749b4898d62661c1ffaa
Yegor <44552545+Rood1e@users.noreply.github.com>
2019-04-20 14:05:29 +0300

    Add files via upload

---
+++ alice_app-master/main_function.py
@@ -0,0 +1,6 @@
+from little_function import *
+
+
+def handle_dialog(request, response, user_storage):
+    output_message = "hello"
+    return message_return(response, user_storage, output_message)
