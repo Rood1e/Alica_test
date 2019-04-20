626e7cffe2cee158fe9e3d227fcb3e020808f011
Yegor <44552545+Rood1e@users.noreply.github.com>
2019-04-20 14:37:45 +0300

    Add files via upload

--- flask_app.py
+++ flask_app.py
@@ -7,20 +7,20 @@ from main_function import *
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 session_storage = {}
-logging.basicConfig(level=logging.DEBUG)
+#logging.basicConfig(level=logging.DEBUG)
 
 
 @app.route('/', methods=["POST"])
 def main():
     alice_request = AliceRequest(request.json)
     alice_response = AliceResponce(alice_request)
-    logging.info("Request {}".format(alice_request))
+    #logging.info("Request {}".format(alice_request))
 
     alice_response, session_storage[alice_response.user_id] = handle_dialog(
         alice_request, alice_response, session_storage.get(alice_request.user_id)
     )
 
-    logging.info("Responce {}".format(alice_response))
+    #logging.info("Responce {}".format(alice_response))
 
     return alice_response.dumps()
 
